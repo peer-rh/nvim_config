8f272757689b5dc1823c5ea9535f2f0bc9f2f36f
Peer Rheinboldt <prheinboldt@student.ethz.ch>
2024-01-03 11:43:28 +0100

    format and lazy lock

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,213 +1,213 @@
 return {
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
-        lazy = true,
-        config = false,
-        init = function()
-            local lsp_zero = require("lsp-zero")
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		lazy = true,
+		config = false,
+		init = function()
+			local lsp_zero = require("lsp-zero")
 
-            -- Disable automatic setup, we are doing it manually
-            vim.g.lsp_zero_extend_cmp = 0
-            vim.g.lsp_zero_extend_lspconfig = 0
+			-- Disable automatic setup, we are doing it manually
+			vim.g.lsp_zero_extend_cmp = 0
+			vim.g.lsp_zero_extend_lspconfig = 0
 
-            lsp_zero.set_sign_icons({
-                error = "",
-                warn = "",
-                hint = "",
-                info = "",
-            })
-            require("which-key").register({
-                c = {
-                    "+code",
-                    R = { vim.lsp.buf.references, "References" },
-                    r = { vim.lsp.buf.rename, "Rename" },
-                },
-            }, { prefix = "<leader>" })
-            vim.keymap.set("n", "gd", function()
-                vim.lsp.buf.definition()
-            end)
-            vim.keymap.set("n", "K", function()
-                vim.lsp.buf.hover()
-            end)
-            vim.diagnostic.config({
-                virtual_text = true,
-            })
-        end,
-    },
-    {
-        "williamboman/mason.nvim",
-        dependencies = {
-            { "williamboman/mason-lspconfig.nvim" },
-        },
-        lazy = false,
-        config = true,
-    },
+			lsp_zero.set_sign_icons({
+				error = "",
+				warn = "",
+				hint = "",
+				info = "",
+			})
+			require("which-key").register({
+				c = {
+					"+code",
+					R = { vim.lsp.buf.references, "References" },
+					r = { vim.lsp.buf.rename, "Rename" },
+				},
+			}, { prefix = "<leader>" })
+			vim.keymap.set("n", "gd", function()
+				vim.lsp.buf.definition()
+			end)
+			vim.keymap.set("n", "K", function()
+				vim.lsp.buf.hover()
+			end)
+			vim.diagnostic.config({
+				virtual_text = true,
+			})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+		lazy = false,
+		config = true,
+	},
 
-    -- Autocompletion
-    {
-        "hrsh7th/nvim-cmp",
-        event = { "VeryLazy", "InsertEnter" },
-        dependencies = {
-            { "hrsh7th/cmp-nvim-lsp" }, -- Required
-            { "hrsh7th/cmp-buffer" },  -- Optional
-            { "hrsh7th/cmp-path" },    -- Optional
-            { "saadparwaiz1/cmp_luasnip" }, -- Optional
-            { "hrsh7th/cmp-nvim-lua" }, -- Optional
-            { "L3MON4D3/LuaSnip" },    -- Required
-            { "rafamadriz/friendly-snippets" }, -- Optional
-            { "onsnails/lspkind.nvim" },
-        },
-        config = function()
-            -- Here is where you configure the autocompletion settings.
-            local lspkind = require("lspkind")
-            local lsp_zero = require("lsp-zero")
-            lsp_zero.extend_cmp()
+	-- Autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		event = { "VeryLazy", "InsertEnter" },
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "hrsh7th/cmp-buffer" }, -- Optional
+			{ "hrsh7th/cmp-path" }, -- Optional
+			{ "saadparwaiz1/cmp_luasnip" }, -- Optional
+			{ "hrsh7th/cmp-nvim-lua" }, -- Optional
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "rafamadriz/friendly-snippets" }, -- Optional
+			{ "onsails/lspkind.nvim" },
+		},
+		config = function()
+			-- Here is where you configure the autocompletion settings.
+			local lspkind = require("lspkind")
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.extend_cmp()
 
-            -- And you can configure cmp even more, if you want to.
-            local cmp = require("cmp")
+			-- And you can configure cmp even more, if you want to.
+			local cmp = require("cmp")
 
-            cmp.setup({
-                formatting = {
-                    format = lspkind.cmp_format({
-                        with_text = false,
-                        maxwidth = 50,
-                    }),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-j>"] = cmp.mapping.select_next_item(),
-                    ["<C-k>"] = cmp.mapping.select_prev_item(),
-                    ["<C-l>"] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                }),
-            })
-        end,
-    },
+			cmp.setup({
+				formatting = {
+					format = lspkind.cmp_format({
+						with_text = false,
+						maxwidth = 50,
+					}),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-l>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete(),
+				}),
+			})
+		end,
+	},
 
-    -- LSP
-    {
-        "neovim/nvim-lspconfig",
-        cmd = { "LspInfo", "LspInstall", "LspStart" },
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "williamboman/mason-lspconfig.nvim" },
-        },
-        config = function()
-            -- This is where all the LSP shenanigans will live
-            local lsp_zero = require("lsp-zero")
-            lsp_zero.extend_lspconfig()
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+		config = function()
+			-- This is where all the LSP shenanigans will live
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.extend_lspconfig()
 
-            lsp_zero.on_attach(function(_, bufnr)
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
-                lsp_zero.default_keymaps({ buffer = bufnr })
-            end)
+			lsp_zero.on_attach(function(_, bufnr)
+				-- see :help lsp-zero-keybindings
+				-- to learn the available actions
+				lsp_zero.default_keymaps({ buffer = bufnr })
+			end)
 
-            require("mason-lspconfig").setup({
-                ensure_installed = {},
-                handlers = {
-                    lsp_zero.default_setup,
-                    lua_ls = function()
-                        -- (Optional) Configure lua language server for neovim
-                        local lua_opts = lsp_zero.nvim_lua_ls()
-                        require("lspconfig").lua_ls.setup(lua_opts)
-                    end,
-                },
-            })
-        end,
-    },
-    {
-        "github/copilot.vim",
-        lazy = true,
-        event = { "InsertEnter", "VeryLazy" },
-        config = function()
-            vim.cmd([[Copilot enable]])
-        end,
-    },
-    {
-        "akinsho/flutter-tools.nvim",
-        lazy = true,
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = true,
-    },
-    {
-        "windwp/nvim-ts-autotag",
-        lazy = true,
-        event = "InsertEnter",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                autotag = {
-                    enable = true,
-                },
-            })
-        end,
-    },
-    {
-        "stevearc/conform.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local conform = require("conform")
+			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				handlers = {
+					lsp_zero.default_setup,
+					lua_ls = function()
+						-- (Optional) Configure lua language server for neovim
+						local lua_opts = lsp_zero.nvim_lua_ls()
+						require("lspconfig").lua_ls.setup(lua_opts)
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"github/copilot.vim",
+		lazy = true,
+		event = { "InsertEnter", "VeryLazy" },
+		config = function()
+			vim.cmd([[Copilot enable]])
+		end,
+	},
+	{
+		"akinsho/flutter-tools.nvim",
+		lazy = true,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = true,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		lazy = true,
+		event = "InsertEnter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				autotag = {
+					enable = true,
+				},
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local conform = require("conform")
 
-            conform.setup({
-                formatters_by_ft = {
-                    javascript = { "prettier" },
-                    typescript = { "prettier" },
-                    javascriptreact = { "prettier" },
-                    typescriptreact = { "prettier" },
-                    svelte = { "prettier" },
-                    css = { "prettier" },
-                    html = { "prettier" },
-                    json = { "prettier" },
-                    yaml = { "prettier" },
-                    markdown = { "prettier" },
-                    graphql = { "prettier" },
-                    lua = { "stylua" },
-                    python = { "isort", "black" },
-                },
-                format_on_save = {
-                    lsp_fallback = true,
-                    async = false,
-                    timeout_ms = 500,
-                },
-            })
-        end,
-    },
-    {
-        "nvimdev/lspsaga.nvim",
-        event = { "VeryLazy" },
-        keys = {
-            { "<leader>ce", "<cmd>Lspsaga outline<cr>",              desc = "Symbols" },
-            { "<leader>ci", "<cmd>Lspsaga incoming_calls<cr>",       desc = "Incoming Calls" },
-            { "<leader>co", "<cmd>Lspsaga outgoing_calls<cr>",       desc = "Outgoing Calls" },
-            { "<leader>ca", "<cmd>Lspsaga code_action<cr>",          desc = "Code Action" },
-            { "<leader>cc", "<cmd>Lspsaga diagnostic_jump_prev<cr>", desc = "Prev Diagnostic" },
-            { "<leader>cd", "<cmd>Lspsaga diagnostic_jump_next<cr>", desc = "Next Diagnostic" },
-            { "<leader>cf", "<cmd>Lspsaga finder<cr>",               desc = "Finder" },
-        },
-        config = function()
-            require("lspsaga").setup({
-                symbol_in_winbar = {
-                    enable = false,
-                },
-                lightbulb = {
-                    enable = false,
-                },
-                ui = {
-                    code_action = "",
-                },
-                code_action = {
-                    show_server_name = true,
-                    keys = {
-                        quit = "<esc>",
-                        exec = "<cr>",
-                    },
-                },
-            })
-        end,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter", -- optional
-            "nvim-tree/nvim-web-devicons", -- optional
-        },
-    },
+			conform.setup({
+				formatters_by_ft = {
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					svelte = { "prettier" },
+					css = { "prettier" },
+					html = { "prettier" },
+					json = { "prettier" },
+					yaml = { "prettier" },
+					markdown = { "prettier" },
+					graphql = { "prettier" },
+					lua = { "stylua" },
+					python = { "isort", "black" },
+				},
+				format_on_save = {
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 500,
+				},
+			})
+		end,
+	},
+	{
+		"nvimdev/lspsaga.nvim",
+		event = { "VeryLazy" },
+		keys = {
+			{ "<leader>ce", "<cmd>Lspsaga outline<cr>", desc = "Symbols" },
+			{ "<leader>ci", "<cmd>Lspsaga incoming_calls<cr>", desc = "Incoming Calls" },
+			{ "<leader>co", "<cmd>Lspsaga outgoing_calls<cr>", desc = "Outgoing Calls" },
+			{ "<leader>ca", "<cmd>Lspsaga code_action<cr>", desc = "Code Action" },
+			{ "<leader>cc", "<cmd>Lspsaga diagnostic_jump_prev<cr>", desc = "Prev Diagnostic" },
+			{ "<leader>cd", "<cmd>Lspsaga diagnostic_jump_next<cr>", desc = "Next Diagnostic" },
+			{ "<leader>cf", "<cmd>Lspsaga finder<cr>", desc = "Finder" },
+		},
+		config = function()
+			require("lspsaga").setup({
+				symbol_in_winbar = {
+					enable = false,
+				},
+				lightbulb = {
+					enable = false,
+				},
+				ui = {
+					code_action = "",
+				},
+				code_action = {
+					show_server_name = true,
+					keys = {
+						quit = "<esc>",
+						exec = "<cr>",
+					},
+				},
+			})
+		end,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter", -- optional
+			"nvim-tree/nvim-web-devicons", -- optional
+		},
+	},
 }
